055cc7fb628e1e8beb43436c4b10d3172802dab0
Ngoc <thnhngc17@gmail.com>
2023-07-25 15:53:13 +0700

    Initial Commit

---
+++ Contact List/Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_ListApp.swift
+//  Contact List
+//
+//  Created by Duong Vu Thanh Ngoc on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
